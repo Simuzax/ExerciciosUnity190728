01e934986510eb03ba80174ce48475bf5c2d57ac
Simuzax <simuzax@gmail.com>
2019-07-17 11:54:31 -0300

    190717

---
+++ Exercicios/Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Timers;
+
+public class Game : MonoBehaviour
+{
+    public GameObject inimigoPrefab;
+
+    void spawnInimigo()
+    {
+        GameObject go = Instantiate(inimigoPrefab, getRandomSpawnPosition(), Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimersManager.SetLoopableTimer(this, 2f, spawnInimigo);
+    }
+
+
+    Vector3 getRandomSpawnPosition()
+    {
+        return new Vector3(Random.Range(0, 10), 1, Random.Range(0, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Exercicios/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = -2f;
+
+    bool x2;
+    bool x3;
+    bool x4;
+
+    [SerializeField]
+    Transform cameraT;
+
+    [SerializeField]
+    CharacterController charController;
+
+    [SerializeField]
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walk();
+    }
+
+    void walk()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+
+        Vector3 direction = input.normalized;
+
+        Vector3 velocity = direction * speed;
+
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+    }
+}
